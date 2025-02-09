ab83f97c0cfdb08c70037f14d70dba70a8b87511
Karel Vyborny <149906910+KarelVyborny@users.noreply.github.com>
2025-02-09 22:16:29 +0100

    newbranch

--- LeaveManagementSystem.Web/Models/TestViewModel.cs
+++ LeaveManagementSystem.Web/Models/TestViewModel.cs
@@ -3,6 +3,6 @@
     public class TestViewModel
     {
         public string Name { get; set; }
-        public DateTime? DateOfCost { get; set; }
+        public DateTime? CostDate { get; set; }
     }
 }
