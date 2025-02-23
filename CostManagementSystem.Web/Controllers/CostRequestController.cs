8920075e1b1628c192ab7afd132ef8454f2f5e54
Karel Vyborny <149906910+KarelVyborny@users.noreply.github.com>
2025-02-23 18:29:22 +0100

    new theme

--- CostManagementSystem.Web/Controllers/CostRequestController.cs
+++ CostManagementSystem.Web/Controllers/CostRequestController.cs
@@ -11,7 +11,6 @@ using Microsoft.EntityFrameworkCore;
 
 namespace CostManagementSystem.Web.Controllers
 {
-    [Authorize]
     public class CostRequestController(ICostRequestService _costRequestService, ApplicationDbContext _context) : Controller
     {
         //View Request
